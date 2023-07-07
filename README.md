a1abe0fdb73f471470b12b0eda55228f71eb2015
Florian Bramer <florian.bramer@esrlabs.com>
2023-07-07 17:50:36 +0200

    Improved usability
    It is no longer necessary to modify the shell script.
    
    Radio station URL and output device can now be specified
    on the command line.
    
    The microphone device is detected automatically.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# C.A.R.P.
+# carp
 
 _Call Aware Radio Player_
 
@@ -11,73 +11,41 @@ Tested on Ubuntu Linux with:
 - MS Teams
 - Slack Hangouts
 
-C.A.R.P. is implemented in a single shell script.
+carp is implemented in a single shell script.
 
 ## Preconditions
 
 - Linux with Pulse Audio Environment
 - _mpg123_ and _pactl_ installed.
 
-  On Ubuntu:
-  `sudo apt install mpg123 pulseaudio-utils`
+On Ubuntu you can satisfy the preconditions with
+
+`sudo apt install mpg123 pulseaudio-utils`
 
 
-## Setup
+## Working Principle
 
-`carp.sh` is a shell script which polls the status of your Microphone every second.
+`carp.sh` is a shell script which polls the status of your microphone(s) every second.
 
-- whenever the microphone becomes used, `carp.sh` assumes that a call has started.
-- whenever it becomes available again, `carp.sh` assumes that the call has ended.
+- whenever a microphone becomes used, `carp.sh` assumes that a call has started.
+- whenever all microphones become available again, `carp.sh` assumes that the call has ended.
 
-In order to pause / resume a radio station it needs to know three things from you:
-
-
-### 1. Streaming URL of a radio station
-
-You can lookup the streaming URL of your preferred radio station on the Internet.
-
-For example at https://streamurl.link/
-
-My favorite radio station is [Radio FM4](https://fm4.orf.at/). Its streaming URL is the default in `carp.sh`. Modify this line with your URL:
-
-```
-RADIO_STATION_URL="http://ors-sn06.ors-shoutcast.at/fm4-q2a"
-```
-
-### 2. Pulse audio name of your microphone source
-
-The pulse audio name of your microphone can be looked up with this command line:
-
-```
-pactl list short sources
-```
-
-In case the command lists more than one device you might need to try them out until you find the right one.
-
-Exchange this value in `carp.sh` with your microphone device:
-
-```
-CALL_MICROPHONE_DEVICE="alsa_input.usb-0b0e_Jabra_Link_370_50C2ED094FE8-00.mono-fallback"
-```
-
-### 3. Pulse audio name of your output device for playing radio
-
-I usually do my calls with a Jabra headset. But for listening to music, I prefer some real speakers. Therefore you can explicitlly define the output sink for radio.
-
-Similar to the lookup of the microphone device, you can lookup your output devices.
-
-```
-pactl list short sinks
-```
-
-Exchange this value in `carp.sh` with your preferred output device:
-
-```
-RADIO_OUTPUT_DEVICE="alsa_output.pci-0000_01_00.1.hdmi-stereo"
-```
+In order to pause / resume a radio station it needs to know two things from you:
 
 ## Execution
 
-Just run `./carp.sh` in your terminal.
+Just run `./carp.sh` in your terminal. It will offer you a list of predefined radio stations and play on your default output device.
 
+Use these available options to override the default behaviour:
 
+```
+./carp.sh --help
+Call Aware Radio Player
+
+USAGE:
+ ./carp.sh [OPTIONS]
+
+OPTIONS:
+-u, --url <radio_stream>     Radio stream to play (see https://streamurl.link/)
+-o, --output <pa_device>     Pulse Audio device to play on (see 'pactl list short sinks')
+```
--- carp.sh
+++ carp.sh
@@ -1,40 +1,90 @@
 #!/bin/bash
 #
-# C.A.R.P.: a Call Aware Radio Player
+# carp:  Call Aware Radio Player
 #
 # see https://github.com/elektronenhirn/call_aware_radio_player
 #
 
-# Your favorite radio stations' streaming URL. Lookup at https://streamurl.link/
-RADIO_STATION_URL="http://ors-sn06.ors-shoutcast.at/fm4-q2a"
-
-# Output device for radio, lookup with 'pactl list short sinks'
-RADIO_OUTPUT_DEVICE="alsa_output.pci-0000_01_00.1.hdmi-stereo"
-
-# Input device for calls, lookup with 'pactl list short sources'
-CALL_MICROPHONE_DEVICE="alsa_input.usb-0b0e_Jabra_Link_370_50C2ED094FE8-00.mono-fallback"
-
+RADIO_STATION_URL=
 POLLING_INTERVAL_IN_SEC=1
 RADIO_PID=
 CALL_ONGOING=
 
-COLOR='\033[1;32m'
-NOCOLOR='\033[0m' # No Color
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NOCOLOR='\033[0m'
 
 function log() {
     TIMESTAMP=$(date +"%Y-%m-%d %T")
-    echo -e "${COLOR}${TIMESTAMP}  $*${NOCOLOR}"
+    echo -e "${GREEN}${TIMESTAMP}  $*${NOCOLOR}"
 }
+
+function error() {
+    echo -e "${RED}$*${NOCOLOR}"
+}
+
+function printHelp() {
+    echo "Call Aware Radio Player"
+    echo ""
+    echo "USAGE:"
+    echo " $0 [OPTIONS]"
+    echo ""
+    echo "OPTIONS:"
+    echo "-u, --url <radio_stream>     Radio stream to play (see https://streamurl.link/)"
+    echo "-o, --output <pa_device>     Pulse Audio device to play on (see 'pactl list short sinks')"
+    echo ""
+}
+
 function playRadio() {
-    mpg123 -o pulse -a ${RADIO_OUTPUT_DEVICE} ${RADIO_STATION_URL} &
+    OUTPUT_DEVICE_SEGMENT=
+    if [ "${OUTPUT_DEVICE}" != "" ]; then
+        OUTPUT_DEVICE_SEGMENT="-a ${OUTPUT_DEVICE}"
+    fi
+    mpg123 -o pulse ${OUTPUT_DEVICE_SEGMENT} ${STREAMING_URL} &
     RADIO_PID=$!
 }
 
 # returns 0 when call ongoing
 function callOngoing() {
-    REGEX="${CALL_MICROPHONE_DEVICE}.*RUNNING"
+    REGEX=".*RUNNING"
     pactl list short sources | grep -e "${REGEX}" > /dev/null
     echo $?
+}
+
+function selectRadioStation() {
+    RADIO_STATION_NAME[1]="FM4 from Vienna/Austria"
+    RADIO_STATION_URL[1]="http://ors-sn06.ors-shoutcast.at/fm4-q2a"
+
+    RADIO_STATION_NAME[2]="KEXP from Seattle/USA"
+    RADIO_STATION_URL[2]="http://kexp-mp3-128.streamguys1.com/kexp128.mp3"
+
+    RADIO_STATION_NAME[3]="NBC from Bolzano/Italy"
+    RADIO_STATION_URL[3]="http://s6.mediastreaming.it:7020/;"
+
+    PS3='Select a radio station: '
+
+    options=("${RADIO_STATION_NAME[1]}", "${RADIO_STATION_NAME[2]}", "${RADIO_STATION_NAME[3]}")
+    select opt in "${options[@]}"
+    do
+        case $REPLY in
+            1)
+                echo "→ Selected ${RADIO_STATION_NAME[1]}"
+                STREAMING_URL="${RADIO_STATION_URL[1]}"
+                break
+                ;;
+            2)
+                echo "→ Selected ${RADIO_STATION_NAME[2]}"
+                STREAMING_URL="${RADIO_STATION_URL[2]}"
+                break
+                ;;
+            3)
+                echo "→ Selected ${RADIO_STATION_NAME[3]}"
+                STREAMING_URL="${RADIO_STATION_URL[3]}"
+                break
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
 }
 
 # $1 call state to handle (0 -> call ongoing)
@@ -48,7 +98,51 @@ function handleCallState() {
     fi
 }
 
-log "Welcome to C.A.R.P. // press [CTRL+C] to stop.."
+
+# see https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -u|--url)
+      STREAMING_URL="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -o|--outputdevice)
+      OUTPUT_DEVICE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -h|--help)
+      printHelp
+      shift # past argument
+      exit 0
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+if [ "${STREAMING_URL}" = "" ]; then
+    echo "No streaming URL given on the command line, pick a radio station from the list:"
+    selectRadioStation
+fi
+
+if [ "${OUTPUT_DEVICE}" = "" ]; then
+    echo "No output device given on the command line"
+    echo "→ using system's default"
+fi
+
+echo "Press [CTRL+C] to stop playing or monitoring call state"
 
 for (( ; ; ))
 do
